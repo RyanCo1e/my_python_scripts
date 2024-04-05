df6ee7a5dcedd27f9fe17075a392c5f65218bbc8
RyanCo1e <164986818+RyanCo1e@users.noreply.github.com>
2024-04-05 22:15:28 +0630

    Add files via upload

---
+++ banner_grab.py
@@ -0,0 +1,20 @@
+#/usr/bin/python3
+#Developed by nlh
+import socket
+from termcolor import colored
+
+def banner(ip, port):
+        try:
+                s = socket.socket()
+                s.connect((ip, port))
+                data = s.recv(1024)
+                return data
+        except:
+                return 
+
+host = input(colored("Enter the hostname you want to connect: ", "blue"))
+host_ip = socket.gethostbyname(host)
+
+for port in range(1,1000):
+        banner_data = banner(host_ip, port)
+        print(f"{banner_data} is received from port {port}")
